8eb177b997d9a3d47e063263af2ace35cf3f1389
Mirona108 <mironaws@yahoo.com>
2019-06-19 09:42:06 +0300

    Added new code

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,13 @@
 package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Pet;
-import com.example.petclinic.services.CrudService;
+import com.example.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+@Service
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
@@ -23,7 +25,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements Crud
 
     @Override
     public Pet save(Pet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,13 @@
 package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Vet;
-import com.example.petclinic.services.CrudService;
+import com.example.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+@Service
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -23,7 +25,7 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements Crud
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
